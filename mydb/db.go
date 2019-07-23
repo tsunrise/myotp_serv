65c5a9d4048d450adf53751e8114446239ff5c8c
Tom Shen <tomshen@berkeley.edu>
2019-07-23 16:08:18 +0800

    add stmts

--- mydb/db.go
+++ mydb/db.go
@@ -7,35 +7,43 @@ import (
 	"log"
 )
 
-func InitDB() (*sql.DB, error) {
+func InitDB() (*sql.DB, *StatementsSet, error) {
+	// establish connection
 	info, err := ParseJSONtoDBInfo()
 	if err != nil {
-		return nil, newDbError(err.Error())
+		return nil, nil, newDbError(err.Error())
 	}
 	src := fmt.Sprintf("%v:%v@tcp(%v)/%v", info.AppUserName, info.AppUserPassword, info.SqlAddr, info.DatabaseName)
 	db, err := sql.Open("mysql", src)
 
 	if err != nil {
-		return nil, newDbError("Fail to connect database: " + err.Error())
+		return nil, nil, newDbError("Fail to connect database: " + err.Error())
 	}
 
 	log.Println("Database connection has been established: " +
 		fmt.Sprintf("%v@tcp(%v)/%v", info.AppUserName, info.SqlAddr, info.DatabaseName))
 
+	// show table lists
 	rows, err := db.Query("show tables")
 	if err != nil {
-		return nil, newDbError("Fail to access table list: " + err.Error())
+		return nil, nil, newDbError("Fail to access table list: " + err.Error())
 	}
 	var result string
 	for rows.Next() {
 		err = rows.Scan(&result)
 		if err != nil {
-			return nil, newDbError("Fail to access table values: " + err.Error())
+			return nil, nil, newDbError("Fail to access table values: " + err.Error())
 		}
 		log.Println("Database has table: " + result)
 	}
 
-	return db, nil
+	// making statements
+	stmts, err := NewStatements(db)
+	if err != nil {
+		return nil, nil, newDbError("Fail to make prepared statements: " + err.Error())
+	}
+
+	return db, stmts, nil
 
 }
 
---
+++ mydb/statements.go
@@ -0,0 +1,36 @@
+package mydb
+
+import "database/sql"
+
+type StatementsSet struct {
+	NewUser     *sql.Stmt //name
+	DeleteUser  *sql.Stmt //user_id
+	PromoteUser *sql.Stmt //user_id (set privilege to 1)
+	FireUser    *sql.Stmt //user_id (set privilege to 0)
+}
+
+func NewStatements(db *sql.DB) (*StatementsSet, error) {
+	newUser, err := db.Prepare("insert into users(`name`) values (?);")
+	if err != nil {
+		return nil, err
+	}
+	deleteUser, err := db.Prepare("delete from users where user_id=?")
+	if err != nil {
+		return nil, err
+	}
+	promoteUser, err := db.Prepare("update users set privilege=1 where user_id=?")
+	if err != nil {
+		return nil, err
+	}
+	fireUser, err := db.Prepare("update users set privilege=0 where user_id=?")
+	if err != nil {
+		return nil, err
+	}
+
+	return &StatementsSet{
+		NewUser:     newUser,
+		DeleteUser:  deleteUser,
+		PromoteUser: promoteUser,
+		FireUser:    fireUser,
+	}, nil
+}
