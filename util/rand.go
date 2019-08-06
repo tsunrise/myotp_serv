06e71bb63be26bf4cb524ca4988e2193d3de922a
Tom Shen <tomshen@berkeley.edu>
2019-08-06 20:41:15 +0800

    true random

--- util/rand.go
+++ util/rand.go
@@ -1,11 +1,16 @@
 package util
 
-import "math/rand"
+import (
+	"math/rand"
+	"os"
+	"time"
+)
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 // @source https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
 func RandStringBytesRmndr(n int) string {
+	rand.Seed(int64(os.Getpid()+os.Geteuid()+os.Getppid()+os.Getegid()) + time.Now().Unix())
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
