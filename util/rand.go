220b349d206d88262ec2fde2b328951ca7d4384f
Tom Shen <tomshen@berkeley.edu>
2019-08-07 20:26:24 +0800

    optimize random
    set random src

--- util/rand.go
+++ util/rand.go
@@ -10,10 +10,13 @@ const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 // @source https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
 func RandStringBytesRmndr(n int) string {
-	rand.Seed(int64(os.Getpid()+os.Geteuid()+os.Getppid()+os.Getegid()) + time.Now().Unix())
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
 	}
 	return string(b)
+}
+
+func setRandomSource() {
+	rand.Seed(int64(os.Getpid()+os.Geteuid()+os.Getppid()+os.Getegid()) + time.Now().UnixNano())
 }
